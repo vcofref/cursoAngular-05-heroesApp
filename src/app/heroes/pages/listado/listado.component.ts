f040df683bedf55c3b623c7b12bbd75da9b1862b
Victor Cofre Farias <victor.cofre.f@gmail.com>
2021-03-15 10:05:09 -0300

    ajustes

---
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styles: [
+  ]
+})
+export class ListadoComponent implements OnInit {
+
+  constructor( private heroesService : HeroesService ) { }
+
+  ngOnInit(): void {
+    
+    this.heroesService.getHeroes()
+    .subscribe( resp => console.log( resp ) );
+  }
+
+}
